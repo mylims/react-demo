be590a03e915cea254f7e53f4b2c101516fef04e
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-12-04 12:42:41 +0100

    feat: add JCAMP content

--- src/components/ExtractedInfo.tsx
+++ src/components/ExtractedInfo.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from 'react';
-import { fromCVd } from 'iv-spectrum';
+import { fromCVd, toJcamp } from 'iv-spectrum';
 import { Plot, LineSeries, XAxis, YAxis } from 'react-plot';
 
 import MetaTable from './MetaTable';
@@ -40,119 +40,137 @@ export default function ExtractedInfo({ text }: InfoProps) {
     [analysis, variablesState],
   );
 
-  return (
-    <div className="flex flex-wrap justify-center">
-      {meta && <MetaTable meta={meta} />}
-      <div className="m-2 shadow sm:rounded-lg">
-        {variables && (
-          <Plot
-            width={500}
-            height={500}
-            margin={{ bottom: 50, left: 80, top: 20, right: 20 }}
-          >
-            <LineSeries
-              data={{ x: variables.x.data, y: variables.y.data }}
-              displayMarker={plotState.showMarkers}
-            />
-            <XAxis
-              label={`${variables.x.label} [${variables.x.units}]`}
-              showGridLines={plotState.showGridLines}
-            />
-            <YAxis
-              label={`${variables.y.label} [${variables.y.units}]`}
-              showGridLines={plotState.showGridLines}
-              labelSpace={50}
-            />
-          </Plot>
-        )}
-      </div>
-      <div className="w-48 m-2 shadow sm:rounded-lg">
-        <div className="p-2 m-2">
-          <Select
-            label="X variable"
-            options={Object.keys(allVariables).map((label) => ({
-              label,
-              value: label,
-            }))}
-            selected={{
-              label: variablesState.xLabel,
-              value: variablesState.xLabel,
-            }}
-            onSelect={(selected) => {
-              const { label = 'Vd' } = selected || {};
-              setVariablesState({
-                ...variablesState,
-                xLabel: label,
-                xUnits: allVariables[label] || 'V',
-              });
-            }}
-          />
-          <Input
-            name="xUnits"
-            label="X units"
-            placeholder="X units"
-            value={variablesState.xUnits}
-            onChange={(e) => {
-              const { value: xUnits = 'V' } = e.currentTarget;
-              setVariablesState({ ...variablesState, xUnits });
-            }}
-          />
-        </div>
+  const jcamp = useMemo(() => toJcamp(analysis), [analysis]);
 
-        <div className="p-2 m-2">
-          <Select
-            label="Y variable"
-            options={Object.keys(allVariables).map((label) => ({
-              label,
-              value: label,
-            }))}
-            selected={{
-              label: variablesState.yLabel,
-              value: variablesState.yLabel,
-            }}
-            onSelect={(selected) => {
-              const { label = 'C' } = selected || {};
-              setVariablesState({
-                ...variablesState,
-                yLabel: label,
-                yUnits: allVariables[label] || 'fF',
-              });
-            }}
-          />
-          <Input
-            name="yUnits"
-            label="Y units"
-            placeholder="Y units"
-            value={variablesState.yUnits}
-            onChange={(e) => {
-              const { value: yUnits = 'fF' } = e.currentTarget;
-              setVariablesState({ ...variablesState, yUnits });
-            }}
-          />
+  return (
+    <>
+      <div className="flex flex-wrap justify-center">
+        {meta && <MetaTable meta={meta} />}
+        <div className="m-2 shadow sm:rounded-lg">
+          {variables && (
+            <Plot
+              width={500}
+              height={500}
+              margin={{ bottom: 50, left: 80, top: 20, right: 20 }}
+            >
+              <LineSeries
+                data={{ x: variables.x.data, y: variables.y.data }}
+                displayMarker={plotState.showMarkers}
+              />
+              <XAxis
+                label={`${variables.x.label} [${variables.x.units}]`}
+                showGridLines={plotState.showGridLines}
+              />
+              <YAxis
+                label={`${variables.y.label} [${variables.y.units}]`}
+                showGridLines={plotState.showGridLines}
+                labelSpace={50}
+              />
+            </Plot>
+          )}
         </div>
-        <div className="p-2 m-2">
-          <Toggle
-            label="Show grid lines"
-            activated={plotState.showGridLines}
-            onToggle={() => {
-              setPlotState({
-                ...plotState,
-                showGridLines: !plotState.showGridLines,
-              });
-            }}
-          />
-          <Toggle
-            label="Show markers"
-            activated={plotState.showMarkers}
-            onToggle={() => {
-              setPlotState({
-                ...plotState,
-                showMarkers: !plotState.showMarkers,
-              });
-            }}
-          />
+        <div className="w-48 m-2 shadow sm:rounded-lg">
+          <div className="p-2 m-2">
+            <Select
+              label="X variable"
+              options={Object.keys(allVariables).map((label) => ({
+                label,
+                value: label,
+              }))}
+              selected={{
+                label: variablesState.xLabel,
+                value: variablesState.xLabel,
+              }}
+              onSelect={(selected) => {
+                const { label = 'Vd' } = selected || {};
+                setVariablesState({
+                  ...variablesState,
+                  xLabel: label,
+                  xUnits: allVariables[label] || 'V',
+                });
+              }}
+            />
+            <Input
+              name="xUnits"
+              label="X units"
+              placeholder="X units"
+              value={variablesState.xUnits}
+              onChange={(e) => {
+                const { value: xUnits = 'V' } = e.currentTarget;
+                setVariablesState({ ...variablesState, xUnits });
+              }}
+            />
+          </div>
+
+          <div className="p-2 m-2">
+            <Select
+              label="Y variable"
+              options={Object.keys(allVariables).map((label) => ({
+                label,
+                value: label,
+              }))}
+              selected={{
+                label: variablesState.yLabel,
+                value: variablesState.yLabel,
+              }}
+              onSelect={(selected) => {
+                const { label = 'C' } = selected || {};
+                setVariablesState({
+                  ...variablesState,
+                  yLabel: label,
+                  yUnits: allVariables[label] || 'fF',
+                });
+              }}
+            />
+            <Input
+              name="yUnits"
+              label="Y units"
+              placeholder="Y units"
+              value={variablesState.yUnits}
+              onChange={(e) => {
+                const { value: yUnits = 'fF' } = e.currentTarget;
+                setVariablesState({ ...variablesState, yUnits });
+              }}
+            />
+          </div>
+          <div className="p-2 m-2">
+            <Toggle
+              label="Show grid lines"
+              activated={plotState.showGridLines}
+              onToggle={() => {
+                setPlotState({
+                  ...plotState,
+                  showGridLines: !plotState.showGridLines,
+                });
+              }}
+            />
+            <Toggle
+              label="Show markers"
+              activated={plotState.showMarkers}
+              onToggle={() => {
+                setPlotState({
+                  ...plotState,
+                  showMarkers: !plotState.showMarkers,
+                });
+              }}
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <div>
+        <div className="flex flex-wrap justify-center">
+          {jcamp && (
+            <div className="p-5 m-2 shadow sm:rounded-lg">
+              <div className="text-xl font-semibold text-blue-500 border-b">
+                JCAMP
+              </div>
+              <div className="inline-block max-w-2xl overflow-auto border-gray-200 h-96">
+                {jcamp}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
