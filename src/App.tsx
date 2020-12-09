befa5d01851b522e2baf582059d966e71cca3d3d
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-12-09 14:34:36 +0100

    chore: refactor routes definition and add samples

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { SidebarLayout } from './components/tailwind-ui';
-import Title from './components/Title';
+import { Title, paths } from './components/Title';
 
 import IVCurve from './views/IVCurve';
+import SamplesTable from './views/SamplesTable';
 
 export default function App() {
-  const itemClass =
-    'flex items-center w-full py-2 text-sm font-medium rounded-md cursor-pointer group hover:text-neutral-900 hover:bg-neutral-100 text-neutral-600';
   return (
     <Router>
       <SidebarLayout>
@@ -15,9 +14,14 @@ export default function App() {
           <div className="flex flex-col flex-grow mt-5">
             <nav className="flex-1 px-2 space-y-1 bg-white">
               <ul>
-                <li className={itemClass}>
-                  <Link to="/ivcurve">IV spectra</Link>
-                </li>
+                {Object.keys(paths).map((key) => (
+                  <li
+                    key={key}
+                    className="flex items-center w-full py-2 text-sm font-medium rounded-md cursor-pointer group hover:text-neutral-900 hover:bg-neutral-100 text-neutral-600"
+                  >
+                    <Link to={key}>{paths[key] || 'Demo'}</Link>
+                  </li>
+                ))}
               </ul>
             </nav>
           </div>
@@ -29,6 +33,9 @@ export default function App() {
           <Switch>
             <Route path="/ivcurve">
               <IVCurve />
+            </Route>
+            <Route path="/samplestable">
+              <SamplesTable />
             </Route>
             <Route path="/*">
               <IVCurve />
---
+++ src/views/SamplesTable.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SamplesTable() {
+  return <div>test</div>;
+}
