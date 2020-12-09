431bdfb04a16ecafa28c8c9323a6825b705599ac
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-12-09 14:34:36 +0100

    chore: refactor routes definition and add samples

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function Title() {
+export const paths: Record<string, string | undefined> = {
+  '/ivcurve': 'IV spectra',
+  '/samplestable': 'Samples table',
+};
+
+export function Title() {
   const location = useLocation();
-  const paths: Record<string, string | undefined> = {
-    '/ivcurve': 'IV spectra',
-  };
   return (
     <h1 className="m-4 text-xl font-bold text-primary-500">
       {paths[location.pathname] || 'Demo'}
