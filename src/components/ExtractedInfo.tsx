31342bd888486f8990e25020b4171396248c138f
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-12-15 05:44:01 +0100

    chore: reorganize plot modifiers with plot

--- src/components/ExtractedInfo.tsx
+++ src/components/ExtractedInfo.tsx
@@ -46,114 +46,120 @@ export default function ExtractedInfo({ text }: InfoProps) {
     <>
       <div className="flex flex-wrap justify-center">
         {meta && <MetaTable meta={meta} />}
-        <div className="m-2 shadow sm:rounded-lg">
-          {variables && (
-            <Plot
-              width={500}
-              height={500}
-              margin={{ bottom: 50, left: 80, top: 20, right: 20 }}
-            >
-              <LineSeries
-                data={{ x: variables.x.data, y: variables.y.data }}
-                displayMarker={plotState.showMarkers}
-              />
-              <XAxis
-                label={`${variables.x.label} [${variables.x.units}]`}
-                showGridLines={plotState.showGridLines}
-              />
-              <YAxis
-                label={`${variables.y.label} [${variables.y.units}]`}
-                showGridLines={plotState.showGridLines}
-                labelSpace={50}
-              />
-            </Plot>
-          )}
-        </div>
-        <div className="w-48 m-2 shadow sm:rounded-lg">
-          <div className="p-2 m-2">
-            <Select
-              label="X variable"
-              options={Object.keys(allVariables).map((label) => ({
-                label,
-                value: label,
-              }))}
-              selected={{
-                label: variablesState.xLabel,
-                value: variablesState.xLabel,
-              }}
-              onSelect={(selected) => {
-                const { label = 'Vd' } = selected || {};
-                setVariablesState({
-                  ...variablesState,
-                  xLabel: label,
-                  xUnits: allVariables[label] || 'V',
-                });
-              }}
-            />
-            <Input
-              name="xUnits"
-              label="X units"
-              placeholder="X units"
-              value={variablesState.xUnits}
-              onChange={(e) => {
-                const { value: xUnits = 'V' } = e.currentTarget;
-                setVariablesState({ ...variablesState, xUnits });
-              }}
-            />
+        <div className="flex flex-wrap">
+          <div className="m-2 shadow sm:rounded-lg">
+            {variables && (
+              <Plot
+                width={500}
+                height={500}
+                margin={{ bottom: 50, left: 80, top: 20, right: 20 }}
+              >
+                <LineSeries
+                  data={{ x: variables.x.data, y: variables.y.data }}
+                  displayMarker={plotState.showMarkers}
+                />
+                <XAxis
+                  label={`${variables.x.label} [${variables.x.units}]`}
+                  showGridLines={plotState.showGridLines}
+                />
+                <YAxis
+                  label={`${variables.y.label} [${variables.y.units}]`}
+                  showGridLines={plotState.showGridLines}
+                  labelSpace={50}
+                />
+              </Plot>
+            )}
           </div>
+          <div className="w-48 m-2 shadow sm:rounded-lg">
+            <div className="p-2 m-2">
+              <Select
+                className="w-full"
+                label="X variable"
+                options={Object.keys(allVariables).map((label) => ({
+                  label,
+                  value: label,
+                }))}
+                selected={{
+                  label: variablesState.xLabel,
+                  value: variablesState.xLabel,
+                }}
+                onSelect={(selected) => {
+                  const { label = 'Vd' } = selected || {};
+                  setVariablesState({
+                    ...variablesState,
+                    xLabel: label,
+                    xUnits: allVariables[label] || 'V',
+                  });
+                }}
+              />
+              <Input
+                className="w-full"
+                name="xUnits"
+                label="X units"
+                placeholder="X units"
+                value={variablesState.xUnits}
+                onChange={(e) => {
+                  const { value: xUnits = 'V' } = e.currentTarget;
+                  setVariablesState({ ...variablesState, xUnits });
+                }}
+              />
+            </div>
 
-          <div className="p-2 m-2">
-            <Select
-              label="Y variable"
-              options={Object.keys(allVariables).map((label) => ({
-                label,
-                value: label,
-              }))}
-              selected={{
-                label: variablesState.yLabel,
-                value: variablesState.yLabel,
-              }}
-              onSelect={(selected) => {
-                const { label = 'C' } = selected || {};
-                setVariablesState({
-                  ...variablesState,
-                  yLabel: label,
-                  yUnits: allVariables[label] || 'fF',
-                });
-              }}
-            />
-            <Input
-              name="yUnits"
-              label="Y units"
-              placeholder="Y units"
-              value={variablesState.yUnits}
-              onChange={(e) => {
-                const { value: yUnits = 'fF' } = e.currentTarget;
-                setVariablesState({ ...variablesState, yUnits });
-              }}
-            />
-          </div>
-          <div className="p-2 m-2">
-            <Toggle
-              label="Show grid lines"
-              activated={plotState.showGridLines}
-              onToggle={() => {
-                setPlotState({
-                  ...plotState,
-                  showGridLines: !plotState.showGridLines,
-                });
-              }}
-            />
-            <Toggle
-              label="Show markers"
-              activated={plotState.showMarkers}
-              onToggle={() => {
-                setPlotState({
-                  ...plotState,
-                  showMarkers: !plotState.showMarkers,
-                });
-              }}
-            />
+            <div className="p-2 m-2">
+              <Select
+                className="w-full"
+                label="Y variable"
+                options={Object.keys(allVariables).map((label) => ({
+                  label,
+                  value: label,
+                }))}
+                selected={{
+                  label: variablesState.yLabel,
+                  value: variablesState.yLabel,
+                }}
+                onSelect={(selected) => {
+                  const { label = 'C' } = selected || {};
+                  setVariablesState({
+                    ...variablesState,
+                    yLabel: label,
+                    yUnits: allVariables[label] || 'fF',
+                  });
+                }}
+              />
+              <Input
+                className="w-full"
+                name="yUnits"
+                label="Y units"
+                placeholder="Y units"
+                value={variablesState.yUnits}
+                onChange={(e) => {
+                  const { value: yUnits = 'fF' } = e.currentTarget;
+                  setVariablesState({ ...variablesState, yUnits });
+                }}
+              />
+            </div>
+            <div className="p-2 m-2">
+              <Toggle
+                label="Show grid lines"
+                activated={plotState.showGridLines}
+                onToggle={() => {
+                  setPlotState({
+                    ...plotState,
+                    showGridLines: !plotState.showGridLines,
+                  });
+                }}
+              />
+              <Toggle
+                label="Show markers"
+                activated={plotState.showMarkers}
+                onToggle={() => {
+                  setPlotState({
+                    ...plotState,
+                    showMarkers: !plotState.showMarkers,
+                  });
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>
