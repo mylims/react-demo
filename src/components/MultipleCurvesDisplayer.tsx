2054ee2c92f905d87e98b0b47e16a00ee010f32c
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-03-30 15:50:01 +0200

    fix: show right lables on superimposed spectra

--- src/components/MultipleCurvesDisplayer.tsx
+++ src/components/MultipleCurvesDisplayer.tsx
@@ -3,6 +3,7 @@ import { fromMulChannelCap } from 'iv-spectrum';
 import { getReactPlotJSON } from 'common-spectrum';
 import { PlotObject, PlotObjectType } from 'react-plot';
 import { schemeRdYlBu } from 'd3-scale-chromatic';
+import { ReactPlotOptions } from 'common-spectrum/lib/reactPlot/getReactPlotJSON';
 
 interface MultipleProps {
   textList: string[];
@@ -10,9 +11,8 @@ interface MultipleProps {
 
 const colorScheme = schemeRdYlBu[11].concat().reverse();
 const query = { xLabel: 'Vd', xUnits: 'V', yLabel: 'Id', yUnits: 'mA' };
-const margin = { bottom: 50, left: 80, top: 20, right: 120 };
-const options = {
-  legend: { position: 'right' },
+const margin = { bottom: 50, left: 80, top: 20, right: 130 };
+const options: ReactPlotOptions = {
   series: { displayMarker: false },
   dimensions: { width: 700, height: 500, margin },
 };
@@ -22,7 +22,7 @@ export default function MultipleCurvesDisplayer({ textList }: MultipleProps) {
   useEffect(() => {
     const analyses = textList.map((text) => fromMulChannelCap(text));
     const data = getReactPlotJSON(analyses, query, options);
-    setData({ ...data, colorScheme });
+    setData({ ...data, colorScheme, legend: { position: 'right' } });
   }, [textList]);
 
   return data && <PlotObject plot={data} />;
